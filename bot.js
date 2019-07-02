02536bc31bcf1e21b7746c4c5d8c77b4aa5bf0e0
Paulino Mota hernandez <paulinomotahernandez@Paulinos-MBP-2.xalapa.intra.uv.mx>
2019-07-02 12:46:24 -0500

    se agrega respuesta de hola

--- bot.js
+++ bot.js
@@ -8,7 +8,16 @@ class MyBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            await context.sendActivity(`You said '${ context.activity.text }'`);
+            
+            console.log(context);
+            console.log(" =============== ")
+            console.log(context.activity);
+            
+            if(context.activity.text.toUpperCase() == "HOLA"){  
+                return await context.sendActivity(`Que onda bro`);    
+            }
+
+            await context.sendActivity(`tu escribiste '${ context.activity.text }'`);
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
@@ -17,7 +26,7 @@ class MyBot extends ActivityHandler {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Hello and welcome!');
+                    await context.sendActivity('Hola, es bueno verte!');
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
