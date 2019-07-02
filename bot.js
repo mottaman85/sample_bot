ea53f1dc3280ee82780c1bfad3f7d71bfde49e60
Paulino Mota hernandez <paulinomotahernandez@Paulinos-MBP-2.xalapa.intra.uv.mx>
2019-07-02 13:15:59 -0500

    se agrega un card de video

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler } = require('botbuilder');
+const { ActivityHandler, MessageFactory, CardFactory} = require('botbuilder');
 
 class MyBot extends ActivityHandler {
     constructor() {
@@ -14,10 +14,36 @@ class MyBot extends ActivityHandler {
             console.log(context.activity);
             
             if(context.activity.text.toUpperCase() == "HOLA"){  
-                return await context.sendActivity(`Que onda bro`);    
+
+
+
+                context.sendActivity({ attachments: [CardFactory.videoCard(
+            '2018 Imagine Cup World Championship Intro',
+            [{ url: 'https://sec.ch9.ms/ch9/783d/d57287a5-185f-4df9-aa08-fcab699a783d/IC18WorldChampionshipIntro2.mp4' }],
+            [{
+                type: 'openUrl',
+                title: 'Lean More',
+                value: 'https://channel9.msdn.com/Events/Imagine-Cup/World-Finals-2018/2018-Imagine-Cup-World-Championship-Intro'
+            }],
+            {
+                subtitle: 'by Microsoft',
+                text: 'Microsoft\'s Imagine Cup has empowered student developers around the world to create and innovate on the world stage for the past 16 years. These innovations will shape how we live, work and play.'
+            }
+        )] })
+
+
+
+
+                var reply = MessageFactory.suggestedActions(['Red', 'Yellow', 'Blue'], 'What is the best color?');
+                return await context.sendActivity(reply);            
+            }else {
+
+var reply = MessageFactory.suggestedActions(['Hola', 'Menu', 'Preguntas frecuentes'], 'Hola, no entiendo ru pregunta, pero puedes teclear esto esto:');
+                return await context.sendActivity(reply);       
+
             }
 
-            await context.sendActivity(`tu escribiste '${ context.activity.text }'`);
+//            await context.sendActivity(`tu escribiste '${ context.activity.text }'`);
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
